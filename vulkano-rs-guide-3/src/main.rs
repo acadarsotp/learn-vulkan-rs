d6adb4fca0f10cf9c5439bb5e1e0c5c1325bfa3c
acada <acadarsotp@gmail.com>
2023-10-18 10:11:02 +0200

    1024 by 1024 blue square

---
+++ vulkano-rs-guide-3/src/main.rs
@@ -0,0 +1,114 @@
+//Code based on the official vulkano guide
+
+use image::{ImageBuffer, Rgba};
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, ClearColorImageInfo, CommandBufferUsage, CopyImageToBufferInfo,
+};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::format::{ClearColorValue, Format};
+use vulkano::image::{ImageDimensions, StorageImage};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryUsage, StandardMemoryAllocator};
+use vulkano::sync;
+use vulkano::sync::GpuFuture;
+
+pub fn main() {
+    let library = vulkano::VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+
+    let physical = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .skip(1)
+        .next()
+        .expect("no devices available");
+
+    let queue_family_index = physical
+        .queue_family_properties()
+        .iter()
+        .enumerate()
+        .position(|(_, q)| q.queue_flags.contains(QueueFlags::GRAPHICS))
+        .expect("couldn't find a graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+    )
+        .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+
+    let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
+    let command_buffer_allocator =
+        StandardCommandBufferAllocator::new(device.clone(), Default::default());
+
+    // Image creation
+    let image = StorageImage::new(
+        &memory_allocator,
+        ImageDimensions::Dim2d {
+            width: 1024,
+            height: 1024,
+            array_layers: 1, // images can be arrays of layers
+        },
+        Format::R8G8B8A8_UNORM,
+        Some(queue.queue_family_index()),
+    )
+        .unwrap();
+
+    // To read the content of the image we need a CPU accessible buffer to which the GPU can copy
+    // the contents of the image
+    let buf = Buffer::from_iter(
+        &memory_allocator,
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            usage: MemoryUsage::Download,
+            ..Default::default()
+        },
+        //1024*1024 pixels, and each pixel is 4 bytes in R8G8B8A8_UNORM
+        (0..1024 * 1024 * 4).map(|_| 0u8),
+    )
+        .expect("failed to create buffer");
+
+    let mut builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+        .unwrap();
+    builder
+        .clear_color_image(ClearColorImageInfo {
+            clear_value: ClearColorValue::Float([0.0, 0.0, 1.0, 1.0]),
+            ..ClearColorImageInfo::image(image.clone())
+        })
+        .unwrap()
+        .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(image, buf.clone()))
+        .unwrap();
+    let command_buffer = builder.build().unwrap();
+
+    let future = sync::now(device)
+        .then_execute(queue, command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(None).unwrap();
+
+    // Exporting the result
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("image.png").unwrap();
+
+    println!("Everything succeeded!");
+}
